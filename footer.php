1be6e0fe12c19eb4eeeaef560cb79515251d0258
Ezra Free <ezrafree@gmail.com>
2016-02-10 07:22:15 -0700

    add sticky footer

--- footer.php
+++ footer.php
@@ -13,13 +13,14 @@
 
 		</div><!-- #content -->
 
-		<footer id="colophon" class="site-footer">
-			<div class="site-info">
-				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'wp-theme' ), 'wp-theme', '<a href="http://www.websightdesigns.com/">websightdesigns</a>' ); ?>
+    </div><!-- #page -->
 
-			</div><!-- .site-info -->
-		</footer><!-- #colophon -->
-	</div><!-- #page -->
+	<footer id="footer" class="site-footer">
+		<div class="container site-info">
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'wp-theme' ), 'wp-theme', '<a href="http://www.websightdesigns.com/">websightdesigns</a>' ); ?>
+
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
 
 <?php wp_footer(); ?>
 
